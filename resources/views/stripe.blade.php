6b077a6ade08dfa8711e733e9ecddcc3674cf6c9
neljoshua01 <njoshuat@gmail.com>
2020-09-18 10:13:46 +0800

    added go back button on stripePayment

--- resources/views/stripe.blade.php
+++ resources/views/stripe.blade.php
@@ -10,6 +10,7 @@
 </head>
 
 <body>
+	<a href="/"><button class="btn btn-primary" style="margin:20px;">Go Back</button></a>
 	<div class="container">
 		<div class='row'>
 			<div class='col-md-4'></div>
